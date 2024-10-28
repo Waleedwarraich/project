ad982277e84f18db21fc6318ead536afaf34305e
Waleedwarraich <s3994931@student.rmit.edu.au>
2024-10-28 18:36:46 +0500

    Logging out pop-up message configured accurately

--- logout.php
+++ logout.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 session_destroy();
+session_start();
+$_SESSION['message'] = [
+    'type' => 'success',
+    'text' => 'Logout Successfully' 
+];
 header("Location: index.php");
 exit();
 ?>
